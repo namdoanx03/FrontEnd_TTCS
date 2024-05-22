5aeab9f56497cac26d7b98d570d9258924f0d2ba
namdoanx03 <dpnbro1102@gmail.com>
2024-05-22 23:38:32 +0700

    init

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,69 +1,82 @@
 import "./login.css";
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import { getUser } from "../../redux/actions";
 import { useDispatch } from "react-redux";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { getUser } from "../../redux/actions";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { Container, Form, Button, InputGroup, Row, Col } from "react-bootstrap";
 
 function Login() {
     const dispatch = useDispatch();
-   
-    const [show ,setShow] = useState(false)
-     const handleClickEye = () =>{
-        setShow( !show )
-     }
+    const [show, setShow] = useState(false);
+    const handleClickEye = () => setShow(!show);
 
     const [user, setUser] = useState({
         username: '',
         password: '',
-    })
+    });
 
-    const handleClickLogin = React.useCallback(() => {
+    const handleClickLogin = useCallback((e) => {
+        e.preventDefault();
         dispatch(getUser.getUserRequest(user));
-  
-
-
         setUser({
             username: '',
             password: '',
-        })
+        });
+    }, [user, dispatch]);
 
-
-
-    }, [user, dispatch])
-    // useEffect(() => {
-    // }, [success])
     return (
-        <>
-            {console.log('render')}
-            <div className="Login" >
-
-                <form action="/action_page.php" className="Login_content">
-                    <h3 className="Login_text">Login here</h3>
-                    <div className="Login_input">
-                        <input value={user.username} onChange={e => setUser({ ...user, username: e.target.value })} placeholder="UserName..." required/>
-                        <br />
-                        <input className="input_parent" type={ show ? 'text' : 'password'  }  value={user.password} onChange={e => setUser({ ...user, password: e.target.value })} placeholder="PassWord..."  required/>
-                        <span className="childrenofInput"> <FontAwesomeIcon onClick={handleClickEye} icon={ show ? faEyeSlash : faEye}/></span>
-
-                    </div>
-                    <h5 className="Login_text_sub"> click the login button </h5>
-                    <div className="Login_btn">
-                        <button className="abcdlogin" onClick={handleClickLogin} >
-                            <span  id="abcd" className="navlink">Login</span>
-                           
-                        </button>
-                        {/* onClick={handleClickLogin} */}
-                        <div className="abcdlogin"><NavLink className="navlink" to="/register">Register</NavLink> </div>
-                    </div>
-                </form>
-            </div>
-
-        </>
-
+        <div className="background-login">
+            <Container fluid className="d-flex justify-content-center align-items-center vh-100 ">
+                <Row className="w-100 justify-content-center">
+                    <Col xs={12} md={6} lg={4}>
+                        <div className="Login">
+                            <Form className="Login_content" onSubmit={handleClickLogin}>
+                                <h3 className="Login_text text-center">Login here</h3>
+                                <Form.Group className="Login_input mb-3">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Username..."
+                                        value={user.username}
+                                        onChange={(e) => setUser({ ...user, username: e.target.value })}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group className="Login_input mb-3">
+                                    <InputGroup>
+                                        <Form.Control
+                                            type={show ? 'text' : 'password'}
+                                            placeholder="Password..."
+                                            value={user.password}
+                                            onChange={(e) => setUser({ ...user, password: e.target.value })}
+                                            required
+                                        />
+                                        <InputGroup.Text onClick={handleClickEye} style={{ cursor: 'pointer' }}>
+                                            <FontAwesomeIcon icon={show ? faEyeSlash : faEye} />
+                                        </InputGroup.Text>
+                                    </InputGroup>
+                                </Form.Group>
+                                <div className="Login_btn text-center">
+                                    <Button type="submit" className="abcdlogin me-2 w-100 my-3">
+                                        Login
+                                    </Button>
+                                    <div>
+                                        <a href="#" className="forgot-password">Forgotten password?</a>
+                                    </div>
+                                </div>
+                                <div className="register-form">
+                                    <div className="create-account text-center">
+                                        <NavLink className="abcdlogin navlink btn btn-success border-top w-75 " to="/register">Create new account</NavLink>
+                                    </div>
+                                </div>
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
+
 export default Login;
